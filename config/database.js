dee43c85c2d76f5b6293cfffd735aed54f27435c
Wisdom Ekpot <wisdomekpot@gmail.com>
2019-12-20 15:14:35 +0100

    feature

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 module.exports = {
-  database: "mongodb://localhost:27017/fxapp",
+  database:
+    "mongodb+srv://wisdom132:spinosky@cluster0-xzpmn.gcp.mongodb.net/fxapp?retryWrites=true&w=majority",
+  // database: "mongodb://localhost:27017/fxapp",
   secret: "password"
 };
